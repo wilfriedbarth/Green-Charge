0bf4d4c5fd99626565da8efe7f5f58a9d6f758f3
Wilfried Barth <wilfried.barth.prof@gmail.com>
2017-06-26 18:41:01 -0500

    fixed index.html

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -1,39 +1,40 @@
 const webpack = require("webpack");
 const path = require ('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = { 
-	entry: path.join(__dirname, "../src/index.js"),
+	entry: {
+    bundle: [ path.join(__dirname, "../src/index.js") ]
+  },
 	output: {
 		path: path.join(__dirname, "../dist"),
-		filename: "bundle.js",
-		publicPath: "/"
+		filename: "[name].js",
 	}, 
-
 	devServer: {
 		compress: true, // enable gzip compression (faster compression).
 		historyApiFallback: true, // enable for react router
 		stats: "minimal", // shows errors
 		proxy: {
 			"/api": "http://localhost:3000" // Forward request to local dev server without errors
-		},
-		publicPath: "/"
-
+		}
 	},
-
 	module: {
 		rules: [
-
-		{
-			test: /\.(js|jsx)$/, // Tests for .js or .jsx
-			exclude: /node_modules/, // Ignores node modules with javascript
-			loader: "babel-loader", // Transpiles code from es6/7 and spits out es5
-			options: {
-				presets: ["env", "react"], // Based on browser enviornment, transpile accordingly
-				cacheDirectory: true //touch
-			}	
-		}
-
+      {
+        test: /\.(js|jsx)$/, // Tests for .js or .jsx
+        exclude: /node_modules/, // Ignores node modules with javascript
+        loader: "babel-loader", // Transpiles code from es6/7 and spits out es5
+        options: {
+          presets: ["env", "react"], // Based on browser enviornment, transpile accordingly
+          cacheDirectory: true //touch
+        }	
+      }
 		]
-	}
+	},
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.join(__dirname, '../src/index.html')
+    })
+  ]
 };
 
