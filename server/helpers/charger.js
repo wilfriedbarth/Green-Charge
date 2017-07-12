83b707896fb3eb8b8aa1f09356f4f1a10d9e4be9
Benjamin Williams <ben@benjaminwilliams.com>
2017-07-11 21:42:02 -0500

    Move functioning charger functions to helper file
    querystring package provides URL encocded data required for particle post operation

--- server/helpers/charger.js
+++ server/helpers/charger.js
@@ -1,4 +1,7 @@
- 
+require('dotenv').config();
+const axios = require('axios');
+const querystring = require('querystring')
+
 exports.predictChargeState = function(data) {
   // check that there are at least five records. If there are, 
   // compute average change over past hour and dispatch new
@@ -31,4 +34,43 @@ exports.predictChargeState = function(data) {
 
 exports.dispatchChargeState = function() {
 
+}
+
+exports.getChargeStatus = function(deviceId) {
+  axios.get(`https://api.particle.io/v1/devices/${deviceId}/status`, 
+    {headers: {
+      'Authorization': 'Bearer ' + process.env.PARTICLE_API,
+      'data': process.env.PARTICLE_API
+    }})
+  .then(function (response) {
+    const status = response.data.result
+    if (!status){
+      return 'off'
+    }
+    else if (status){
+      return 'on'
+  }})
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+// pass deviceID + arg 'on' || 'off'
+exports.setChargeStatus = function(deviceId, arg){
+  axios.post(`https://api.particle.io/v1/devices/${deviceId}/pwr`, 
+    querystring.stringify({
+            access_token: process.env.PARTICLE_API,
+            arg: arg
+    }))
+    .then(function (response) {
+      const status = response.data.return_value
+      if (status === 1 && arg === 'on'){
+        return 'on'
+      } else if (status === 0 && arg === 'off'){
+        return 'off'
+      } else {
+        return 'error'
+    }})
+    .catch(function (error) {
+      console.log(error);
+    });
 }
