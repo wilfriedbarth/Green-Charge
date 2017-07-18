bd741bbf84209e8d778d686852f5ef2cd1ffa57c
Wilfried Barth <wilfried.barth.prof@gmail.com>
2017-07-17 20:08:58 -0500

    fixed main

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import { Grid } from 'semantic-ui-react';
 import Graph from './Graph';
-import SearchBar from './SearchBar';
-import { Container } from 'semantic-ui-react';
-
+import SearchBar from './Search_Bar';
+import Signin from './Signin';
+import Devices from './Devices';
 
 class Main extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authenticated: false
+    }
+  }
+
+  componentWillMount() {
+    if(localStorage.getItem('accessToken')) {
+      this.setState({'authenticated': true}); 
+    } else {
+      this.setState({'authenticated': false});
+    }
+  }
+
   render() {
     return(
-      <div>
-        <h1>Main.js</h1>
-        <SearchBar />
-        <Graph />
-      </div>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={13}>
+              <SearchBar />
+              <Graph />
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Devices />
+            </Grid.Column>
+          </Grid.Row>
+          {!this.state.authenticated &&
+            <Signin />
+            }
+        </Grid>
     )
   }
 }
