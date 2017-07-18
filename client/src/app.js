56c01704100cb3809f4c19bcd2aba5f52557bd70
cecolby11 <cecolby11@users.noreply.github.com>
2017-07-17 19:28:46 -0500

    fix paths and make App a function

--- client/src/app.js
+++ client/src/app.js
@@ -9,25 +9,27 @@ import {
 import { Container } from 'semantic-ui-react';
 
 // pull in components 
-import Main from '../components/Main';
-import Navbar from '../components/Navbar';
-import Signout from '../components/Signout';
-import Signup from '../components/Signup';
+import Main from './components/Main';
+import Navbar from './components/Navbar';
+import Signout from './components/Signout';
+import Signup from './components/Signup';
 
 // export the routes 
-const App = (
-  <Router>
-    <div>
-      <Navbar />
-      <Container>
-        <Switch>
-          <Route exact path='/' component={Main} />
-          <Route exact path='/signout' component={Signout} />
-          <Route exact path='/signup' component={Signup}/>
-        </Switch>
-      </Container>
-    </div>
-  </Router>
-)
+const App = function () {
+  return(
+    <Router>
+      <div>
+        <Navbar />
+        <Container>
+          <Switch>
+            <Route exact path='/' component={Main} />
+            <Route exact path='/signout' component={Signout} />
+            <Route exact path='/signup' component={Signup}/>
+          </Switch>
+        </Container>
+      </div>
+    </Router>
+    );
+}
  
 export default App;
